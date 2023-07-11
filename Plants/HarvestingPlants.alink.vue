202cb918ecc2363ff643e2c4d86555cb76c1ab47
Samuel Jörblad <33666789+jorblad@users.noreply.github.com>
2023-07-11 16:53:20 +0200

    Update HarvestingPlants.alink.vue

--- Plants/HarvestingPlants.alink.vue
+++ Plants/HarvestingPlants.alink.vue
@@ -150,7 +150,7 @@ export default {
         if (!harvestCount || harvestCount <= 0) {
           return;
         }
-        var harvestQuantityMeasure = "count";
+        let harvestQuantityMeasure = "count";
         if ("gram" === harvestCount) {
           harvestQuantityMeasure = "weight";
           console.log(harvestQuantityMeasure)
