4878e6a8dbf64f6ac4ca56495cc284c7f7c82353
Samuel Jörblad <33666789+jorblad@users.noreply.github.com>
2023-08-01 16:12:52 +0200

    Update PlantPlantsFromSeed.alink.vue

--- Plants/PlantPlantsFromSeed.alink.vue
+++ Plants/PlantPlantsFromSeed.alink.vue
@@ -243,7 +243,7 @@ export default {
     handle.defineSlot('com.example.farmos_asset_link.actions.v0.plant_seed_inventory', action => {
       action.type('asset-action');
 
-      console.log('V0.9')
+      console.log('V0.10')
 
       action.showIf(({ asset }) => asset.attributes.status !== 'archived'
           // TODO: Implement a better predicate here...
@@ -288,6 +288,7 @@ export default {
                 attributes: {
                     name: `${plantName}`,
                     status: 'active',
+                    notes: { value: notes },
                 },
                 relationships: {
                     plant_type: {
@@ -353,7 +354,7 @@ export default {
                     name: `Planted ${seedCount} seeds`,
                     timestamp: formatRFC3339(new Date()),
                     status: "done",
-                    notes: { value: notes },
+                    //notes: { value: notes },
                 },
                 relationships: {
                     asset: {
