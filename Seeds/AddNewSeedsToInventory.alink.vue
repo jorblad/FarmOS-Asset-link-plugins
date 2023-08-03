0f03776fc3c7560f3c2527f9426fad5e838d9eff
Samuel Jörblad <33666789+jorblad@users.noreply.github.com>
2023-08-03 08:08:28 +0200

    Cleanup findunitterm

--- Seeds/AddNewSeedsToInventory.alink.vue
+++ Seeds/AddNewSeedsToInventory.alink.vue
@@ -109,34 +109,8 @@ export default {
   async onLoad(handle, assetLink) {
     await assetLink.booted;
 
-    const findUnitTerm = async entitySource => {
-      const results = await entitySource.query(q => q
-          .findRecords('taxonomy_term--unit')
-          .filter({ attribute: 'name', op: 'equal', value: UNIT_NAME }));
-      return results.flatMap(l => l).find(a => a);
-    };
 
-    let seedUnitTerm = await findUnitTerm(assetLink.entitySource.cache);
-
-    if (!seedUnitTerm) {
-      seedUnitTerm = await findUnitTerm(assetLink.entitySource);
-    }
-
-    if (!seedUnitTerm) {
-      const unitTermToCreate = {
-          type: 'taxonomy_term--unit',
-          id: uuidv4(),
-          attributes: {
-            name: UNIT_NAME,
-          },
-      };
-
-      seedUnitTerm = await assetLink.entitySource.update(
-          (t) => t.addRecord(unitTermToCreate),
-          {label: `Add '${UNIT_NAME}' unit`});
-    }
-
-    handle.defineSlot('com.example.farmos_asset_link.actions.v0.increment_seed_inventory', action => {
+    handle.defineSlot('se.sj-tech.farmos_asset_link.actions.v0.increment_seed_inventory', action => {
       action.type('asset-action');
 
       action.showIf(({ asset }) => asset.attributes.status !== 'archived'
@@ -186,9 +160,12 @@ export default {
             },
             units: {
               data: {
-                type: seedUnitTerm.type,
-                id: seedUnitTerm.id,
-              }
+                type: 'taxonomy_term--unit',
+                id: uuidv4(),
+                '$relateByName': {
+                name: UNIT_NAME,
+                },
+    }
             },
           },
         };
