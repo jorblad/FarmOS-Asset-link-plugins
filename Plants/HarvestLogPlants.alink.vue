b9aa80d3a9d8248a53f93bdb2839225870a30c44
Samuel Jörblad <33666789+jorblad@users.noreply.github.com>
2023-08-10 13:34:18 +0200

    Update HarvestLogPlants.alink.vue

--- Plants/HarvestLogPlants.alink.vue
+++ Plants/HarvestLogPlants.alink.vue
@@ -49,6 +49,82 @@ export default {
         console.log('Harvest log: V0.1')
 
         action.showIf(({ log }) => log.attributes.status != `done` );
+        const doActionWorkflow = async (asset) => {
+            const dialogResult = await assetLink.ui.dialog.custom(handle.thisPlugin, { asset });
+            console.log('Dialog result:', dialogResult);
+            const harvestCount = dialogResult.harvestCount;
+            console.log('Harvest Count:', harvestCount);
+            const harvestUnitTerm = dialogResult.quantityType;
+            console.log('QuantityType:', harvestUnitTerm);
+
+            if (!harvestUnitTerm) {
+            return;
+            }
+
+
+            if (!harvestCount || harvestCount <= 0) {
+            return;
+            }
+
+            let harvestQuantityMeasure = "count";
+            if (harvestUnitTerm.attributes.name === "gram" ) {
+            harvestQuantityMeasure = "weight";
+            } else {
+            harvestQuantityMeasure = "count";
+            }
+
+
+
+            const harvestQuantity = {
+            type: 'quantity--standard',
+            id: uuidv4(),
+            attributes: {
+                measure: harvestQuantityMeasure,
+                value: {
+                numerator: harvestCount,
+                denominator: 1,
+                decimal: `${harvestCount}`,
+                },
+            },
+            relationships: {
+                units: {
+                data: {
+                    type: 'taxonomy_term--unit',
+                    id: uuidv4(),
+                    '$relateByName': {
+                    name: UNIT_NAME,
+                    },
+                }
+                },
+            },
+            };
+
+            const harvestLog = {
+            type: props.log.type,
+            id: props.log.id,
+            attributes: {
+                timestamp: formatRFC3339(new Date()),
+                status: "done",
+            },
+            relationships: {
+                quantity: {
+                data: [
+                    {
+                    type: harvestQuantity.type,
+                    id: harvestQuantity.id,
+                    }
+                ]
+                },
+            },
+            };
+
+            assetLink.entitySource.update(
+                (t) => [
+                t.addRecord(harvestQuantity),
+                t.updateRecord(harvestLog),
+                ],
+                {label: `Record harvest`});
+        };
         try {
             action.component(({ log }) =>
                 h(QBtn, { block: true, color: 'secondary', onClick: () => doActionWorkflow(log), 'no-caps': true },  "Harvest" ));
