18d95466e1d44d13bbb1b1fad63be14618f12906
Samuel Jörblad <33666789+jorblad@users.noreply.github.com>
2024-04-12 16:27:33 +0200

    .

--- Plants/HarvestingPlants.alink.vue
+++ Plants/HarvestingPlants.alink.vue
@@ -79,12 +79,7 @@ onMounted(async () => {
   
 });
 
-const childrenFilter = [{
-    attribute: 'parent.id',
-    value: asset.id
-}];
 
-console.log('childrenFilter records:', childrenFilter);
 const quantityType = ref(null);
 const unitLabelFn = unitTerm => unitTerm.attributes.name;
 
