1f09f2cce6f6b3449059b463461b5754e385dac9
Samuel Jörblad <33666789+jorblad@users.noreply.github.com>
2024-04-12 16:19:18 +0200

    .

--- Plants/HarvestingPlants.alink.vue
+++ Plants/HarvestingPlants.alink.vue
@@ -58,12 +58,7 @@ watch(photoCaptureModel, async () => {
   carouselPosition.value = photoId;
 });
 
-const childrenFilter = [{
-    attribute: 'parent.id',
-    value: asset.id
-}];
 
-  console.log('childrenFilter records:', childrenFilter);
 
 const findUnitTerms = async (entitySource) => {
   const results = await entitySource.query((q) =>
@@ -77,6 +72,13 @@ const findUnitTerms = async (entitySource) => {
 
   return unitTerms;
 };
+
+const childrenFilter = [{
+    attribute: 'parent.id',
+    value: asset.id
+}];
+
+console.log('childrenFilter records:', childrenFilter);
 
 const unitTerms = ref([]);
 
