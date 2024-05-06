927585ecdd95586ec7d4ce39a51c5c086272f1f7
Samuel Jörblad <33666789+jorblad@users.noreply.github.com>
2024-05-06 13:42:48 +0200

    .

--- DoneAssetLogs.alink.vue
+++ DoneAssetLogs.alink.vue
@@ -36,7 +36,7 @@ const resolveDoneAssetLogs = async () => {
       .filter({ attribute: 'status', op: '=', value: 'done' })
       .filter({
         relation: 'asset.id',
-        op: 'some',
+        op: 'all',
         records: [{ type: props.asset.type, id: props.asset.id }]
       })
       .sort('timestamp');
