10c55392a56a4886ecde0309923c70319249e29d
Samuel Jörblad <samuel.jorblad@sr.se>
2025-02-10 10:42:14 +0100

    Added mous trap reset

---
+++ Mousetraps/resetTrap.vue
@@ -0,0 +1,179 @@
+<script setup>
+import { inject, ref, onMounted } from 'vue';
+import { useDialogPluginComponent } from 'quasar'
+const assetLink = inject('assetLink');
+const props = defineProps({
+  asset: {
+    type: Object,
+    required: true,
+  },
+});
+
+defineEmits([
+  ...useDialogPluginComponent.emits
+]);
+
+const { dialogRef, onDialogOK, onDialogCancel } = useDialogPluginComponent();
+
+const selectedAction = ref({});
+
+// Photo adding
+const capturedPhotos = ref([]);
+
+const photoCaptureModel = ref(null);
+const carouselPosition = ref("capture-photo");
+
+watch(photoCaptureModel, async () => {
+  const file = photoCaptureModel.value;
+
+  if (!file) {
+    return;
+  }
+
+  const fileToArrayBuffer = data => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = error => reject(error);
+    reader.readAsArrayBuffer(data);
+  });
+
+  const fileName = file.name;
+  const fileType = file.type;
+  const fileData = await fileToArrayBuffer(file);
+
+  const fileStringData = new Uint8Array(fileData).reduce((data, byte) => {
+    return data + String.fromCharCode(byte);
+  }, '');
+
+  const fileDataUrl = `data:${fileType};base64, ` + btoa(fileStringData);
+
+  const photoId = uuidv4()
+
+  capturedPhotos.value.push({
+    id: photoId,
+    fileName,
+    fileDataUrl,
+  });
+
+  carouselPosition.value = photoId;
+});
+
+const onSubmit = () => {
+  onDialogOK({ capturedPhotos: capturedPhotos.value, photoCaptureModel: photoCaptureModel.value });
+};
+</script>
+
+<template>
+  <q-dialog ref="dialogRef" @hide="onDialogHide">
+    <q-card class="q-dialog-plugin q-gutter-md" style="width: 700px; max-width: 80vw;">
+      <h4>Reset trap {{ props.asset.attributes.name }}?</h4>
+      <div class="q-gutter-sm">
+        <q-radio v-model="selectedAction" checked-icon="task_alt" unchecked-icon="panorama_fish_eye" val="reset" label="Reset trap" />
+        <q-radio v-model="selectedAction" checked-icon="task_alt" unchecked-icon="panorama_fish_eye" val="empty" label="Emty trap" />
+        <q-radio v-model="selectedAction" checked-icon="task_alt" unchecked-icon="panorama_fish_eye" val="putToghetherAndEmpty" label="Put together and empty trap" />
+        </div>
+      <div class="q-pa-md">
+          <q-carousel
+            swipeable
+            animated
+            navigation
+            navigation-icon="mdi-radiobox-marked"
+            control-type="flat"
+            control-color="orange"
+            :arrows="false"
+            height="200px"
+            v-model="carouselPosition"
+          >
+            <q-carousel-slide
+				v-for="(capturedPhoto, capturedPhotoIdx) in capturedPhotos"
+            	:key="capturedPhoto.id"
+            	:name="capturedPhoto.id">
+              <q-img
+                :src="capturedPhoto.fileDataUrl"
+                class="rounded-borders full-height"
+                fit="contain"
+              ></q-img>
+            </q-carousel-slide>
+
+            <q-carousel-slide name="capture-photo">
+              <photo-input class="q-pb-xl" v-model="photoCaptureModel"></photo-input>
+            </q-carousel-slide>
+          </q-carousel>
+        </div>
+      
+      <div class="q-pa-sm q-gutter-sm row justify-end">
+        <q-btn color="secondary" label="Cancel" @click="onDialogCancel" />
+        <q-btn
+          color="primary"
+          label="Record"
+          @click="onSubmit"
+          :disabled="harvestCount <= 0"
+        />
+      </div>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script>
+import { h } from 'vue';
+import { QBtn } from 'quasar';
+
+import { formatRFC3339, summarizeAssetNames, uuidv4 } from "assetlink-plugin-api";
+
+
+export default {
+  async onLoad(handle, assetLink) {
+    await assetLink.booted;
+
+
+
+    handle.defineSlot('se.jorblad.farmos_asset_link.actions.v0.resetTrap', action => {
+
+      action.type('asset-action');
+
+      action.showIf(({ asset }) => asset.attributes.status !== 'archived'
+            && (asset.type === 'asset--equipment' ));
+
+
+      const doActionWorkflow = async (asset) => {
+        const dialogResult = await assetLink.ui.dialog.custom(handle.thisPlugin, { asset });
+        console.log('Dialog result:', dialogResult);
+        const selectedAction = dialogResult.selectedAction;
+        console.log('Selected action:', selectedAction);
+
+        // const actionName = selectedAction.attributes.label;
+
+
+        const resetTrapLog = {
+          type: 'log--activity',
+          attributes: {
+            name: `${actionName} ${asset.attributes.name}`,
+            timestamp: formatRFC3339(new Date()),
+            status: "done",
+          },
+          relationships: {
+            asset: {
+              data: [
+                {
+                  type: asset.type,
+                  id: asset.id,
+                }
+              ]
+            },
+          },
+        };
+
+        assetLink.entitySource.update(
+            (t) => [
+              t.addRecord(resetTrapLog),
+            ],
+            {label: `Record reset ${asset.attributes.name}`});
+      };
+
+      action.component(({ asset }) =>
+        h(QBtn, { block: true, color: 'secondary', onClick: () => doActionWorkflow(asset), 'no-caps': true },  "Record Harvest" ));
+    });
+
+  }
+}
+</script>
