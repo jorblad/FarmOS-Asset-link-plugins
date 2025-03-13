04fad599d451b7edaefd64bf2c564ab2203005c4
Samuel Jörblad <samuel.jorblad@sr.se>
2025-03-13 16:03:39 +0100

    add console logging of props

--- Slots/Map.alink.vue
+++ Slots/Map.alink.vue
@@ -17,38 +17,13 @@ const assetLink = inject('assetLink');
 const onMapInitialized = (map) => {
   map.addBehavior("sidePanel");
   map.addBehavior("layerSwitcherInSidePanel");
+  console.log("Map initialized", map);
+  console.log("Props: ", props);
 
   const layer = map.addLayer('geojson', {
     title: 'All Assets',
     url: createDrupalUrl('/assets/geojson/full/all'),
-    style: (feature) => {
-      // Check if the feature corresponds to the asset in props
-      console.log('feature', feature);
-      console.log('props.asset', props.asset);
-      if (feature.get('id') === props.asset.id) {
-        // Return a different style for the asset in props
-        return new ol.style.Style({
-          fill: new ol.style.Fill({
-            color: 'rgba(255, 0, 0, 0.6)', // Red color with transparency
-          }),
-          stroke: new ol.style.Stroke({
-            color: 'rgba(255, 0, 0, 1)', // Red color
-            width: 2,
-          }),
-        });
-      } else {
-        // Return the default style for other assets
-        return new ol.style.Style({
-          fill: new ol.style.Fill({
-            color: 'rgba(0, 0, 255, 0.6)', // Blue color with transparency
-          }),
-          stroke: new ol.style.Stroke({
-            color: 'rgba(0, 0, 255, 1)', // Blue color
-            width: 1,
-          }),
-        });
-      }
-    },
+    colour: '#0000ff',
   });
 
   layer.getSource().on('change', function () {
@@ -79,7 +54,7 @@ const onMapInitialized = (map) => {
 <script>
 export default {
   onLoad(handle) {
-    handle.defineSlot('zse.jorblad.farmos_asset_link.slots.v0.map', actionsSlot => {
+    handle.defineSlot('se.jorblad.farmos_asset_link.slots.v0.map', actionsSlot => {
       actionsSlot.type('page-slot');
       //actionsSlot.weight(200); // Set the weight to determine the order
 
