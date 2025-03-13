0dabc980b0bb1d2d615baa8843bbf787e3215e89
Samuel Jörblad <samuel.jorblad@sr.se>
2025-03-13 14:55:20 +0100

    Add map slot

---
+++ Slots/Map.alink.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { computed, inject, ref, onMounted, onUnmounted } from 'vue';
+
+import { createDrupalUrl } from "assetlink-plugin-api";
+
+import { useRouter } from 'vue-router'
+
+const router = useRouter();
+
+const props = defineProps({
+  asset: {
+    type: Object,
+    required: true,
+  },
+});
+
+const assetLink = inject('assetLink');
+
+const onMapInitialized = (map) => {
+  map.addBehavior("sidePanel");
+  map.addBehavior("layerSwitcherInSidePanel");
+
+  const layer = map.addLayer('geojson', {
+    title: 'All Assets',
+    url: createDrupalUrl('/assets/geojson/full/all')
+  });
+
+  layer.getSource().on('change', function () {
+    map.zoomToVectors();
+  });
+};
+
+</script>
+
+<template alink-slot[se.jorblad.farmos_asset_link.slots.v0.map]="page-slot(weight: 80)">
+  <div>
+    <farm-map @map-initialized="onMapInitialized"></farm-map>
+  </div>
+</template>
